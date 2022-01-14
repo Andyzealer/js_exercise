aac843fc38c3c0f21bc8ecbeea2fb22df19a61b9
Andy <andyzealer@gmail.com>
2022-01-14 17:01:24 +0800

    单文件组件练习

---
+++ Vue学习/19_单文件组件/school.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <h2>studentName: {{ schoolName }}</h2>
+    <h2>schooladdress:{{ address }}</h2>
+    <button @click="showName">click me and give you school name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "school",
+  data() {
+    return {
+      schoolName: "Andy",
+      address: "Beijing",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.schoolName);
+    },
+  },
+};
+</script>
+
+<style lang="">
+.demo {
+  background-color: orange;
+}
+</style>
---
+++ Vue学习/19_单文件组件/student.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>schoolName: {{ name }}</h2>
+    <h2>age:{{ age }}</h2>
+    <button @click="showName" class="demo">click me and give you name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "student",
+  data() {
+    return {
+      name: "Andy",
+      age: 16,
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style lang="">
+.demo {
+  background-color: orange;
+}
+</style>
